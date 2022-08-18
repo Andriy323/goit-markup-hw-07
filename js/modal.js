5dfa3f4f2af8b5b8df4d09306ad1641bfc7e1152
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2022-08-18 14:39:38 +0300

    finish dz 7

--- js/modal.js
+++ js/modal.js
@@ -16,31 +16,6 @@
 
 })();
 
-var h = document.getElementById("test");
-var hh = window.getComputedStyle(h, null).height;
-alert(hh);
-
-var foo = getComputedStyle(document.getElementById("test"));
-alert(foo); 
-
-// document.querySelector('button').onclick =testFunc;
-
-// function testFunc() {
-  
-  
-//   var aUrl = document.getElementById('a').clientHeight;
-
-  
-//   var hImg = document.getElementById('img').clientHeight;
-//   var hP =document.getElementById('p').clientHeight;
-//    var hD = document.getElementById('hD').clientHeight;
-//   var hDall =document.getElementById('hD').offsetHeight;
-//   var px = "px \n";
-
-
-//   alert("картинки - " + hImg+px + "blok p - " + hP+px + "div - " + hD + px + "div all - " + hDall + px);
-
-// }
 
 
 
